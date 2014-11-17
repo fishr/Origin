92fcadacc40ede4d62fbaab0e974fcb52a703802
Hans Susilo <hsusilo@mit.edu>
2014-11-17 11:49:42 -0500

    Added working LCD

---
+++ LCD_working/main.c
@@ -0,0 +1,369 @@
+/**
+  ******************************************************************************
+  * @file    LTDC_ColorKeying/main.c
+  * @author  MCD Application Team
+  * @version V1.0.1
+  * @date    11-November-2013
+  * @brief   This example describes how to enable and use the color keying 
+  *          functionality of LTDC.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup STM32F429I_DISCOVERY_Examples
+  * @{
+  */
+
+/** @addtogroup LTDC_ColorKeying
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#define PRESSED_1    0x00
+#define PRESSED_2    0x01
+#define PRESSED_3    0x02
+#define PRESSED_4    0x03
+#define PRESSED_5    0x04
+#define PRESSED_6    0x05
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+uint8_t ubPressedButton = PRESSED_1;
+
+LTDC_ColorKeying_InitTypeDef   LTDC_colorkeying_InitStruct;
+/* Private function prototypes -----------------------------------------------*/
+#ifndef USE_Delay
+static void delay(__IO uint32_t nCount);
+#endif /* USE_Delay*/
+static void LCD_Config(void);
+static void LCD_AF_GPIOConfig(void);
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief   Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  /*!< At this stage the microcontroller clock setting is already configured, 
+  this is done through SystemInit() function which is called from startup
+  file (startup_stm32f429_439xx.s) before to branch to application main.
+  To reconfigure the default setting of SystemInit() function, refer to
+  system_stm32f4xx.c file
+  */        
+  
+//  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+////    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+//    
+//  GPIO_InitTypeDef GPIO_InitStructure;
+//  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+//  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+//  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+//  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+//  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+//  GPIO_Init(GPIOC, &GPIO_InitStructure);
+//
+//  GPIO_WriteBit(GPIOC, GPIO_Pin_1, Bit_SET);  //osc enable
+  
+  /* LCD Configuration */
+  LCD_Config();
+  
+  /* Enable Layer1 */
+  LTDC_LayerCmd(LTDC_Layer1, ENABLE);
+  
+  /* Reload configuration of Layer1 */
+  LTDC_ReloadConfig(LTDC_IMReload);
+  
+  /* Enable The LCD */
+  LTDC_Cmd(ENABLE);
+  
+  while (1)
+  {
+    LCD_Clear(0xf81f);  //PINK  
+    LCD_SetLayer(LCD_FOREGROUND_LAYER);
+    uint8_t aTextBuffer[50];
+    sprintf((char*)aTextBuffer, "     %s      ", "Haans"); 
+    LCD_DisplayStringLine(LINE(4), (uint8_t*)aTextBuffer);
+    sprintf((char*)aTextBuffer, "  is awesome   "); 
+    LCD_DisplayStringLine(LINE(8), (uint8_t*)aTextBuffer);
+//    LCD_SetLayer(LCD_BACKGROUND_LAYER);
+    delay(10000000);
+    LCD_Clear(0x001f);  //BLUE
+    delay(10000000);
+    
+  }
+}
+/**
+  * @brief LCD configuration.
+  * @note  This function Configure tha LTDC peripheral :
+  *        1) Configure the Pixel Clock for the LCD
+  *        2) Configure the LTDC Timing and Polarity
+  *        3) Configure the LTDC Layer 1 :
+  *           - The frame buffer is located at SDRAM buffer
+  *           - The Layer size configuration : 240x320
+  *        4) Configure the Color Keying for Layer 1.    
+  * @retval
+  *  None
+  */
+
+static void LCD_Config(void)
+{
+  LTDC_InitTypeDef               LTDC_InitStruct;
+  GPIO_InitTypeDef GPIO_InitStructure;
+  
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+  
+  /* Configure NCS in Output Push-Pull mode */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+  
+  /* Configure the LCD Control pins ------------------------------------------*/
+  LCD_CtrlLinesConfig();
+  LCD_ChipSelect(DISABLE);
+  LCD_ChipSelect(ENABLE);
+
+  /* Configure the LCD_SPI interface -----------------------------------------*/
+  LCD_SPIConfig(); 
+ 
+  /* Power on the LCD --------------------------------------------------------*/
+  LCD_PowerOn();
+    
+  /* Enable the LTDC Clock */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_LTDC, ENABLE);
+  
+  /* Enable the DMA2D Clock */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2D, ENABLE); 
+  
+  /* Configure the LCD Control pins */
+  LCD_AF_GPIOConfig();  
+  
+  /* Configure the FMC Parallel interface : SDRAM is used as Frame Buffer for LCD */
+  SDRAM_Init();
+  
+/* Enable Pixel Clock --------------------------------------------------------*/
+
+  /* Configure PLLSAI prescalers for LCD */
+  /* PLLSAI_VCO Input = HSE_VALUE/PLL_M = 1 Mhz */
+  /* PLLSAI_VCO Output = PLLSAI_VCO Input * PLLSAI_N = 192 Mhz */
+  /* PLLLCDCLK = PLLSAI_VCO Output/PLLSAI_R = 192/3 = 64 Mhz */
+  /* LTDC clock frequency = PLLLCDCLK / RCC_PLLSAIDivR = 64/8 = 8 Mhz */
+  RCC_PLLSAIConfig(192, 7, 3);
+  RCC_LTDCCLKDivConfig(RCC_PLLSAIDivR_Div8);
+  
+  /* Enable PLLSAI Clock */
+  RCC_PLLSAICmd(ENABLE);
+  /* Wait for PLLSAI activation */
+  while(RCC_GetFlagStatus(RCC_FLAG_PLLSAIRDY) == RESET)
+  {
+  }
+
+/* LTDC Initialization -------------------------------------------------------*/
+
+  /* Initialize the horizontal synchronization polarity as active low*/
+  LTDC_InitStruct.LTDC_HSPolarity = LTDC_HSPolarity_AL;     
+  /* Initialize the vertical synchronization polarity as active low */  
+  LTDC_InitStruct.LTDC_VSPolarity = LTDC_VSPolarity_AL;     
+  /* Initialize the data enable polarity as active low */ 
+  LTDC_InitStruct.LTDC_DEPolarity = LTDC_DEPolarity_AL;     
+  /* Initialize the pixel clock polarity as input pixel clock */ 
+  LTDC_InitStruct.LTDC_PCPolarity = LTDC_PCPolarity_IPC;
+  
+  /* Timing configuration */
+    /* Configure horizontal synchronization width */     
+  LTDC_InitStruct.LTDC_HorizontalSync = 9;
+  /* Configure vertical synchronization height */
+  LTDC_InitStruct.LTDC_VerticalSync = 1;
+  /* Configure accumulated horizontal back porch */
+  LTDC_InitStruct.LTDC_AccumulatedHBP = 29; 
+  /* Configure accumulated vertical back porch */
+  LTDC_InitStruct.LTDC_AccumulatedVBP = 3;  
+  /* Configure accumulated active width */  
+  LTDC_InitStruct.LTDC_AccumulatedActiveW = 269;
+  /* Configure accumulated active height */
+  LTDC_InitStruct.LTDC_AccumulatedActiveH = 323;
+  /* Configure total width */
+  LTDC_InitStruct.LTDC_TotalWidth = 279; 
+  /* Configure total height */
+  LTDC_InitStruct.LTDC_TotalHeigh = 327;
+  
+  LTDC_Init(&LTDC_InitStruct);
+  
+  /* Configure R,G,B component values for LCD background color */                   
+  LTDC_InitStruct.LTDC_BackgroundRedValue = 0;            
+  LTDC_InitStruct.LTDC_BackgroundGreenValue = 0;          
+  LTDC_InitStruct.LTDC_BackgroundBlueValue = 0; 
+            
+  LTDC_Init(&LTDC_InitStruct);
+  
+/* LTDC initialization end ---------------------------------------------------*/
+
+  LCD_LayerInit();
+  
+  LTDC_DitherCmd(ENABLE);
+}
+/**
+  * @brief GPIO config for LTDC.
+  * @retval
+  *  None
+  */
+static void LCD_AF_GPIOConfig(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+  
+  /* Enable GPIOI, GPIOJ, GPIOG, GPIOF, GPIOH AHB Clocks */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOB | \
+                         RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOD | \
+                         RCC_AHB1Periph_GPIOF | RCC_AHB1Periph_GPIOG, ENABLE);
+
+/* GPIOs Configuration */
+/*
+ +------------------------+-----------------------+----------------------------+
+ +                       LCD pins assignment                                   +
+ +------------------------+-----------------------+----------------------------+
+ |  LCD_TFT R2 <-> PC.12  |  LCD_TFT G2 <-> PA.06 |  LCD_TFT B2 <-> PD.06      |
+ |  LCD_TFT R3 <-> PB.00  |  LCD_TFT G3 <-> PG.10 |  LCD_TFT B3 <-> PG.11      |
+ |  LCD_TFT R4 <-> PA.11  |  LCD_TFT G4 <-> PB.10 |  LCD_TFT B4 <-> PG.12      |
+ |  LCD_TFT R5 <-> PA.12  |  LCD_TFT G5 <-> PB.11 |  LCD_TFT B5 <-> PA.03      |
+ |  LCD_TFT R6 <-> PB.01  |  LCD_TFT G6 <-> PC.07 |  LCD_TFT B6 <-> PB.08      |
+ |  LCD_TFT R7 <-> PG.06  |  LCD_TFT G7 <-> PD.03 |  LCD_TFT B7 <-> PB.09      |
+ -------------------------------------------------------------------------------
+          |  LCD_TFT HSYNC <-> PC.06  | LCDTFT VSYNC <->  PA.04 |
+          |  LCD_TFT CLK   <-> PG.07  | LCD_TFT DE   <->  PF.10 |
+           -----------------------------------------------------
+
+*/
+
+ /* GPIOA configuration */
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource4, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource11, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource12, GPIO_AF_LTDC);
+
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_6 | \
+                             GPIO_Pin_11 | GPIO_Pin_12;
+                             
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+  
+ /* GPIOB configuration */  
+  GPIO_PinAFConfig(GPIOB, GPIO_PinSource0, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOB, GPIO_PinSource1, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOB, GPIO_PinSource9, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOB, GPIO_PinSource10, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOB, GPIO_PinSource11, GPIO_AF_LTDC);
+
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_8 | \
+                             GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11;
+  
+  GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+ /* GPIOC configuration */
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource7, GPIO_AF_LTDC);
+//  GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_LTDC);
+  
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;// | GPIO_Pin_10;
+                             
+  GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+ /* GPIOD configuration */
+  GPIO_PinAFConfig(GPIOD, GPIO_PinSource3, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOD, GPIO_PinSource6, GPIO_AF_LTDC);
+  
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_6;
+                             
+  GPIO_Init(GPIOD, &GPIO_InitStruct);
+  
+ /* GPIOF configuration */
+  GPIO_PinAFConfig(GPIOF, GPIO_PinSource10, GPIO_AF_LTDC);
+  
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10;
+                             
+  GPIO_Init(GPIOF, &GPIO_InitStruct);     
+
+ /* GPIOG configuration */  
+  GPIO_PinAFConfig(GPIOG, GPIO_PinSource6, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOG, GPIO_PinSource7, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOG, GPIO_PinSource10, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOG, GPIO_PinSource11, GPIO_AF_LTDC);
+  GPIO_PinAFConfig(GPIOG, GPIO_PinSource12, GPIO_AF_LTDC);
+
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_10 | \
+                             GPIO_Pin_11 | GPIO_Pin_12;
+  
+  GPIO_Init(GPIOG, &GPIO_InitStruct);
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+#ifndef USE_Delay
+/**
+  * @brief  Inserts a delay time.
+  * @param  nCount: specifies the delay time length.
+  * @retval None
+  */
+static void delay(__IO uint32_t nCount)
+{
+  __IO uint32_t index = 0; 
+  for(index = nCount; index != 0; index--)
+  {
+  }
+}
+#endif /* USE_Delay*/
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
